935e75d8f8016bf98621d092d04a29c144610996
João Queiroz <joao.pires.queiroz@gmail.com>
2022-06-12 19:04:33 +0100

    Add cards on home page

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Card from 'react-bootstrap/Card'  
 
 function Home() {
   const navigate = useNavigate();
@@ -8,6 +9,25 @@ function Home() {
       <button onClick={() => navigate("/add_word")}>
         Add Word
       </button>
+      <h1>Words of the day</h1>
+      <ul>
+        {['joao', 'joana'].map((name) =>
+          <Card
+            border='primary'
+            key='primary'
+            style={{ width: '18rem' }}
+            className='mb-2'
+            onClick={() => alert(`Hello from here: ${name}`)}
+          >
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Text>
+                {name} translation.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
+      </ul>
     </>
   );
 }
