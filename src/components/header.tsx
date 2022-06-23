1a16c962ab75aab1aaa41d0e5a0fbe01fa66c2c2
João <joao.pires.queiroz@gmail.com>
2022-06-23 01:24:38 +0100

    Improve header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,32 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 
 export default function Header(){
+
+  {/* <Typography noWrap variant="h6" color="inherit" component="a" href="/" sx={{ display: 'flex', textDecoration: 'none', }}>
+    Vocabulary
+  </Typography>
+  <Typography noWrap variant="h6" color="inherit" component="a" href="/word_list" sx={{ flexGrow: 1, textDecoration: 'none', }}>
+    Words
+  </Typography> */}
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <AppBar position="static">
         <Toolbar>
-          <Typography noWrap variant="h6" color="inherit" component="a" href="/" sx={{ display: 'flex', textDecoration: 'none', }}>
-            Vocabulary
-          </Typography>
-          <Typography noWrap variant="h6" color="inherit" component="a" href="/word_list" sx={{ flexGrow: 1, textDecoration: 'none', }}>
-            Words
-          </Typography>
-          <Button color="inherit" href="/register">Register</Button>
-          <Button color="inherit" href="/login">Login</Button>
-          <Button color="inherit" href="/logout">Logout</Button>
+          <Box sx={{ flexGrow: 1, display: 'flex' }}>
+            <Button color="inherit" href="/"> Home </Button>
+            <Button color="inherit" href="/word_list"> Words </Button>
+          </Box>
+          <Button color="inherit" variant="text" href="/register">Register</Button>
+          <Button color="inherit" variant="outlined" href="/login">Login</Button>
+          <Button color="inherit" variant="outlined" href="/logout">Logout</Button>
         </Toolbar>
       </AppBar>
     </Box>
