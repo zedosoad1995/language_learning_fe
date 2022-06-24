4b5b4421eca81182142d2272639013281f775d74
João <joao.pires.queiroz@gmail.com>
2022-06-24 19:56:24 +0100

    Remove button in Words of the Day cards, navigate to word detail when clicked

--- src/components/wordsOfTheDay.tsx
+++ src/components/wordsOfTheDay.tsx
@@ -5,15 +5,24 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
+import { Navigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 
 function WordsOfTheDay({words}: {words: Array<any>}) {
+  const navigate = useNavigate()
+
+  const wordDetails = (e: any) => {
+    const id = e.currentTarget.getAttribute('data-index')
+    navigate(`/word/${id}`)
+  }
+
 	return (
     <>
-      <Typography variant='h4' sx={{fontWeight: 600}}>Words of the day</Typography>
+      <Typography variant='h4' sx={{fontWeight: 600, m: 3}}>Words of the day</Typography>
       <Stack spacing={2}>
-        {words.map((word, counter) =>
-          <Card raised sx={{maxWidth: 500}}>
+        {words.map((word) =>
+          <Card raised sx={{maxWidth: 500}} key={word.id} data-index={word.id} onClick={(e) => wordDetails(e)}>
             <CardContent>
               <Typography variant="h5" component="div">
                 {word['original_word']}
@@ -22,9 +31,6 @@ function WordsOfTheDay({words}: {words: Array<any>}) {
                 {word['translated_word']}
               </Typography>
             </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
           </Card>
         )}
       </Stack>
