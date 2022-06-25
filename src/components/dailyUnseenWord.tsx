427f8a07bf23bde51f4b782ed2f9adb782e9b1f4
João <joao.pires.queiroz@gmail.com>
2022-06-25 15:49:28 +0100

    Refactor word details card

--- src/components/dailyUnseenWord.tsx
+++ src/components/dailyUnseenWord.tsx
@@ -1,22 +1,11 @@
-import Typography from '@mui/material/Typography'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import Checkbox from '@mui/material/Checkbox'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Rating from '@mui/material/Rating'
 import Button from '@mui/material/Button'
-import CardActions from '@mui/material/CardActions'
-import Divider from '@mui/material/Divider'
 import { useEffect, useState } from "react"
 import httpRequest from '../services/httpRequest'
+import WordCard from './wordCard'
 
 
-function DailyUnseenWord({words, onUpdateWords}: {words: Array<any>, onUpdateWords: any}) {
+function TestCard({words, onUpdateWords}: {words: Array<any>, onUpdateWords: any}) {
 	const [word, setWord]: [any, any] = useState()
-	const [knowledge, setKnowledge] = useState(1)
-	const [relevance, setRelevance] = useState(1)
-	const [wordLearned, setWordLearned] = useState(false)
-
 
 	const getUnseenWord = () => {
 		const unseenWord = words.find((word: any) => !word.is_seen)
@@ -25,19 +14,16 @@ function DailyUnseenWord({words, onUpdateWords}: {words: Array<any>, onUpdateWor
 			return
 		}
 
-		setWordLearned(false)
 		setWord(unseenWord)
-		setKnowledge(unseenWord.knowledge)
-		setRelevance(unseenWord.relevance)
 	}
 
 	const nextWord = () => {
 		if(word && 'id' in word){
 			const data = {
-				knowledge,
-				relevance,
+				knowledge: word.knowledge,
+				relevance: word.relevance,
 				is_seen: true,
-				is_learned: wordLearned
+				is_learned: word.is_learned
 			}
 
 			httpRequest('PATCH', `words/${word.id}/`, data)
@@ -53,49 +39,8 @@ function DailyUnseenWord({words, onUpdateWords}: {words: Array<any>, onUpdateWor
   }, [JSON.stringify(words)])
 
 	return (
-    <>
-			{word &&
-				<Card key={word.id}>
-					<CardContent>
-						<Typography variant="h5">
-							{word['original_word']}
-						</Typography>
-						<Typography color="text.secondary">
-							{word['translated_word']}
-						</Typography>
-						<Divider sx={{my: 2}}/>
-						<Typography>
-							Knowledge
-						</Typography>
-						<Rating
-							value={knowledge}
-							onChange={(_, newValue: any) => {
-								setKnowledge(newValue);
-							}}
-						/>
-						<Typography>
-							Relevance
-						</Typography>
-						<Rating
-							value={relevance}
-							onChange={(_, newValue: any) => {
-								setRelevance(newValue);
-							}}
-						/>
-						<FormControlLabel 
-							sx={{display: 'block'}}
-							control={
-								<Checkbox onChange={() => {setWordLearned(!wordLearned)}} checked={wordLearned} />
-							} 
-							label='I already know this word' 
-						/>
-					</CardContent>
-					<CardActions>
-						<Button onClick={nextWord}>Next Word</Button>
-					</CardActions>
-				</Card>}
-    </>
+		<WordCard singleWord={word} cardActions={<Button onClick={nextWord}>Next Word</Button>}/>
   );
 }
 
-export default DailyUnseenWord;
+export default TestCard;
---
+++ src/components/wordCard.tsx
@@ -0,0 +1,65 @@
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Checkbox from '@mui/material/Checkbox'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Rating from '@mui/material/Rating'
+import CardActions from '@mui/material/CardActions'
+import Divider from '@mui/material/Divider'
+import { useEffect, useState } from "react"
+
+
+function WordCard({singleWord, cardActions}: any) {
+	const [word, setWord]: [any, any] = useState()
+
+	useEffect(() => {
+		setWord(singleWord)
+	}, [JSON.stringify(singleWord)])
+
+	return (
+    <>
+			{word &&
+				<Card key={word.id}>
+					<CardContent>
+						<Typography variant="h5">
+							{word['original_word']}
+						</Typography>
+						<Typography color="text.secondary">
+							{word['translated_word']}
+						</Typography>
+						<Divider sx={{my: 2}}/>
+						<Typography>
+							Knowledge
+						</Typography>
+						<Rating
+							value={word.knowledge}
+							onChange={(_, newValue: any) => {
+								setWord({...word, knowledge: newValue})
+							}}
+						/>
+						<Typography>
+							Relevance
+						</Typography>
+						<Rating
+							value={word.relevance}
+							onChange={(_, newValue: any) => {
+								setWord({...word, relevance: newValue})
+							}}
+						/>
+						<FormControlLabel 
+							sx={{display: 'block'}}
+							control={
+								<Checkbox onChange={() => {setWord({...word, is_learned: !word.is_learned})}} checked={word.is_learned} />
+							} 
+							label='I already know this word' 
+						/>
+					</CardContent>
+					<CardActions>
+						{cardActions}
+					</CardActions>
+				</Card>}
+    </>
+  );
+}
+
+export default WordCard;
